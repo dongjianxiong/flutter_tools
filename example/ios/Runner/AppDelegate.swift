ed9dcc0439014172a2e3c2d3edabc3deeccb4dd1
dongjianxiong <dongjianxiong@itbox.cn>
2024-09-14 14:19:58 +0800

    feat: iOS 新增生命周期变更通知

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -8,6 +8,15 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+      let rootVc = self.window.rootViewController!
+      let navi = UINavigationController.init(rootViewController: rootVc)
+      self.window.rootViewController = navi
+      DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
+          let flutterVc = FlutterViewController.init()
+          navi.pushViewController(flutterVc, animated: true)
+          GeneratedPluginRegistrant.register(with: flutterVc.pluginRegistry())
+      }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- ios/Classes/CorePlugin.swift
+++ ios/Classes/CorePlugin.swift
@@ -9,11 +9,56 @@ struct CorePluginStruct {
 
 }
 
+
+public class CorePluginWeakDictionary<Key: AnyObject, Value: AnyObject> {
+    public let mapTable: NSMapTable<Key, Value>
+
+    init() {
+        mapTable = NSMapTable<Key, Value>(
+            keyOptions: .weakMemory,
+            valueOptions: .weakMemory,
+            capacity: 0
+        )
+    }
+
+    subscript(key: Key) -> Value? {
+        get { return mapTable.object(forKey: key) }
+        set { mapTable.setObject(newValue, forKey: key) }
+    }
+
+    public func object(forKey: Key?) {
+        mapTable.object(forKey: forKey)
+    }
+
+    public func removeObject(forKey: Key?) {
+        mapTable.removeObject(forKey: forKey)
+    }
+
+    public func count() -> Int {
+        return mapTable.count
+    }
+
+    public func allObjects() -> [Value]? {
+        return mapTable.objectEnumerator()?.allObjects as? [Value]
+    }
+}
+
+
 public class CorePlugin: NSObject, FlutterPlugin {
+    
+    
+    var channel: FlutterEventChannel!
+    var registrar: FlutterPluginRegistrar!
+
+    static var channelHolders = CorePluginWeakDictionary<CorePlugin, FlutterMethodChannel>.init()
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
-      let channel = FlutterMethodChannel(name: CorePluginStruct.itboxCorePlugin, binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: CorePluginStruct.itboxCorePlugin, binaryMessenger: registrar.messenger())
     let instance = CorePlugin()
+      registrar.addApplicationDelegate(instance)
+      registrar.publish(instance)
     registrar.addMethodCallDelegate(instance, channel: channel)
+      channelHolders[instance] = channel
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -34,5 +79,50 @@ public class CorePlugin: NSObject, FlutterPlugin {
       result(FlutterMethodNotImplemented)
     }
   }
+    
+    deinit {
+        print("CorePlugin 销毁")
+    }
 }
+
+
+extension CorePlugin: UIApplicationDelegate {
+     
+
+    // 拦截 AppDelegate 的生命周期事件
+    public func applicationDidBecomeActive(_ application: UIApplication) {
+       // 处理应用进入前台事件
+       print("App became active from AppDelegate")
+        if let channel = CorePlugin.channelHolders[self] {
+            channel.invokeMethod("onAppLifecycleStateChanged", arguments: "foreground")
+        }
+    }
+
+    public func applicationDidEnterBackground(_ application: UIApplication) {
+       // 处理应用进入后台事件
+       print("App entered background from AppDelegate")
+        if let channel = CorePlugin.channelHolders[self] {
+            channel.invokeMethod("onAppLifecycleStateChanged", arguments: "background")
+        }
+    }
+
+    public func applicationWillResignActive(_ application: UIApplication) {
+        // 应用将进入非活跃状态
+        print("App will resign active from AppDelegate")
+    }
+
+    public func applicationWillTerminate(_ application: UIApplication) {
+        // 处理应用进入后台事件
+        print("App will terminate from AppDelegate")
+    }
+
+    public func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        // 处理应用进入后台事件
+        print("App did received memory warning from AppDelegate")
+    }
+
+//    UIApplication.willResignActiveNotification: 应用将进入非活跃状态。
+//    UIApplication.willTerminateNotification: 应用将被终止。
+ }
+
 
